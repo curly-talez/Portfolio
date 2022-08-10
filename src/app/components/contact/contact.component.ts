9576545bae424b35ee08e36ace61b65dc6df4bac
curly-talez <krishnaveni.skps@gmail.com>
2022-08-10 21:56:38 +0530

    added a loader in contact page

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -11,25 +11,31 @@ export class ContactComponent implements OnInit {
   public contactForm!: FormGroup;
   public messageError: boolean = false;
   public messageSuccess: boolean = false;
+  public showLoader: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
     this.contactForm = new FormGroup({
-      name: new FormControl('', [Validators.required, Validators.minLength(1)]),
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
       email: new FormControl('', [Validators.required, Validators.email]),
-      message: new FormControl('', [Validators.required, Validators.minLength(1), Validators.maxLength(50)])
+      message: new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(50)])
     })
   }
 
+  get form() {
+    return this.contactForm.controls;
+  }
+
   submitMessage() {
+    this.showLoader = true;
     emailjs.sendForm('service_c5kc565', 'template_ewateue', '#contact-form', 'oo1v5QE6yC-NgQbqc')
       .then((result: EmailJSResponseStatus) => {
         console.log(result.text);
-        this.messageSuccess = true;
+        setTimeout(() => { this.messageSuccess = true; this.showLoader = false; }, 100)
       }, (error) => {
         console.log(error.text);
-        this.messageError = true
+        setTimeout(() => { this.messageError = true; this.showLoader = false }, 100)
       });
   }
 }
